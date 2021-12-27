b6b62a3bc4fac755a48b4369d1b4dfb517d469d3
Lukas Maier <metalmaster669@gmail.com>
2021-12-27 23:27:58 +0100

    data: Remove redundant line

--- generate_data.py
+++ generate_data.py
@@ -9,7 +9,6 @@ from neuroment2.mixing import Mixer
 @hydra.main(config_path="conf", config_name="config")
 def main(cfg: DictConfig):
     os.chdir(hydra.utils.to_absolute_path("."))
-    OmegaConf.to_container()
     kwargs = OmegaConf.to_container(cfg["Mixer"])  # we only want a raw dict, DictConfig types may be trouble
     a = Mixer(**kwargs)
 
