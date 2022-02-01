48d2cafcba558cc7e441b265b17d267ef615cbe4
Lukas Maier <metalmaster669@gmail.com>
2022-02-01 22:51:31 +0100

    data: Rewrite parse_dataset.py s.t. it is clean and works for all dataset types

--- parse_dataset.py
+++ parse_dataset.py
@@ -1,40 +1,68 @@
 import glob
-import os
-import pickle as pk
-import yaml
-
 import numpy as np
-from librosa.core import audio, load
-import librosa as lb
-
-from neuroment2.utils import delete_by_indices
-import matplotlib.pyplot as plt
+import os
 from shutil import copyfile
 
-os.chdir("data/Medley-solos-DB/")
-type = "validation"
-num_instruments = np.zeros(
-    8,
-)
-for file in glob.glob("*.wav"):
-    if type in file:
-        file_cur = file[16::]
-        instr = int(file_cur.split("-")[1][0])
-        num_instruments[instr] += 1
 
-n_samples_per_instrument = int(np.min(num_instruments))
+##### PARAMETERS ###################
+INPUT_DATASET_DIR = "/media/WindowsPartition/Users/maier/Desktop/datasets/MedleyDB/full_dataset_v1p2"
+OUTPUT_DATASET_DIR = "data/fixed"
+DATASET_TYPES = ["training", "validation", "testing"]
+N_INSTRUMENTS = 8
 
-files_instruments = [[] for x in range(8)]
-files = []
-files_new = []
-for file in glob.glob("*.wav"):
-    if type in file:
-        file_cur = file[16::]
-        instr = int(file_cur.split("-")[1][0])
-        num_instruments[instr] += 1
-        if len(files_instruments[instr]) < n_samples_per_instrument:
-            files_instruments[instr].append(file)
-            copyfile(file, "../dataset_new/" + file[16::])
+# we oversample the underrepresented classes by this factor
+# overrepresented classes will be undersampled s.t. in the end each class has the same number of files
+MAX_OVERSAMPLING_FACTOR = 2.0
+####################################
 
+# create output directory
+os.makedirs(OUTPUT_DATASET_DIR, exist_ok=True)
 
-print(num_instruments)
+# run parsing for all dataset types at once
+for dataset_type in DATASET_TYPES:
+    # example file path:
+    # 'data/Medley-solos-DB/Medley-solos-DB_training-7_0244a60b-91bc-5202-fef2-548395a4ce86.wav'
+
+    # initialize the array with the number of instruments per class
+    num_instruments_per_class = np.zeros(N_INSTRUMENTS,)
+
+    # get number of instruments per class
+    for instrument in range(N_INSTRUMENTS):
+        # get the list of files with current dataset type and instrument
+        file_list = glob.glob(
+            os.path.join(INPUT_DATASET_DIR, "M*_%s-%d_*.wav" % (dataset_type, instrument)),
+        )
+        num_instruments_per_class[instrument] = len(file_list)
+
+    # define the maximum number of samples per instrument that will be taken
+    n_samples_per_instrument = int(np.min(num_instruments_per_class) * MAX_OVERSAMPLING_FACTOR)
+
+    # validity check
+    if n_samples_per_instrument == 0:
+        raise ValueError("n_samples_per_instrument should not become 0!")
+
+    # copy old files to new direction
+    for instrument in range(N_INSTRUMENTS):
+        # get the list of files with current dataset type and instrument
+        file_list = glob.glob(
+            os.path.join(INPUT_DATASET_DIR, "M*_%s-%d_*.wav" % (dataset_type, instrument)),
+        )
+
+        # randomize the list s.t. we get files from different recordings
+        np.random.shuffle(file_list)
+
+        if len(file_list) > n_samples_per_instrument:
+            # undersample the file list
+            file_list = file_list[:n_samples_per_instrument]
+        elif len(file_list) < n_samples_per_instrument:
+            # oversample the file list
+            while len(file_list) < n_samples_per_instrument:
+                file_list += file_list
+
+            # remove the number of files that are too much
+            file_list = file_list[:n_samples_per_instrument]
+
+        # now go through file list of proper length and copy files from old to new dir
+        for file in file_list:
+            file_name_without_dir = os.path.basename(file)
+            copyfile(file, os.path.join(OUTPUT_DATASET_DIR, file_name_without_dir))
